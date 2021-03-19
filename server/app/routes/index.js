e1e15cf2e927678bb14ec2cb6167af39d4e96476
NikunjGoenka <Nikunj.Goenka#dal.ca>
2021-03-19 10:48:52 +0530

    backend for Teacher Comment

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 const userRouter = require('./userRouter');
 const qnaRouter = require('./qnaRouter');
+const teacherCommentsRouter = require('./teacherCommentsRouter');
 
 router.use('/users', userRouter);
 router.use('/qna', qnaRouter);
+router.use('/teacherComments', teacherCommentsRouter);
 
 
 module.exports = router;
