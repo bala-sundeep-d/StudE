2d8ed5fa0bc2969a7a2a3f2a0ab333278ba42e6f
NikunjGoenka <Nikunj.Goenka#dal.ca>
2021-03-19 10:37:39 +0530

    backend for Teacher Comment

---
+++ server/app/models/teacherCommentsModel.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const TeacherCommentSchema = new Schema({
+  teacherId: {
+    type: 'String',
+    required: true,
+    trim: true
+  },
+  comment: {
+    type: 'String',
+    required: true,
+    trim: true
+  },
+  type: {
+    type: 'String',
+    enum: ['TODO', 'Award', 'Notification', 'Doubt'],
+    required: true,
+    trim: true
+  },
+  studentId: {
+    type: 'String',
+    required: true,
+    trim: true
+  },
+  ownerId: {
+    type: 'String',
+    required: true,
+    trim: true
+  }
+});
+
+module.exports = mongoose.model('teacherComment', TeacherCommentSchema, 'teacherComment');
