fd6a160c60810598f1f9148cc63558c421c61e16
Harry Ben Alex <hr751368@dal.ca>
2021-03-21 07:12:16 +0530

    routing discussions feature

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -3,10 +3,13 @@ const router = express.Router();
 const userRouter = require('./userRouter');
 const qnaRouter = require('./qnaRouter');
 const teacherCommentsRouter = require('./teacherCommentsRouter');
+const commentsRouter = require('./commentRouter');
+const discussionRouter = require('./discussionRouter');
 
 router.use('/users', userRouter);
 router.use('/qna', qnaRouter);
 router.use('/teacherComments', teacherCommentsRouter);
-
+router.use('/comments',commentsRouter);
+router.use('/discussions', dicussionsRouter);
 
 module.exports = router;
